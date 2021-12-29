345b0604c31b7a613b459826652c00c3a32a8106
Vadym Svyrydov <avangardsv@gmail.com>
2021-12-29 19:20:14 +0200

    edit cv

--- modules/cv/Cv.tsx
+++ modules/cv/Cv.tsx
@@ -4,30 +4,30 @@ import { MDXRemote } from "next-mdx-remote";
 import styles from "./_cv.module.scss";
 
 interface CV {
-    compiledSource: string;
-    scope: {
-      title: string;
-      slug: string;
-      name: string;
-    };
-  }
-  
-  interface Props {
-    slagProps: CV;
-  }
+  compiledSource: string;
+  scope: {
+    title: string;
+    slug: string;
+    name: string;
+  };
+}
 
-const Cv = ({ slagProps }: Props ) => {
-    return (
-        <>
-        <Head>
-            <title>{slagProps.scope.title}</title>
-        </Head>
-        <div className={styles.container}>
-            <Nav navLinkType="CUSTOM_NAV_LINK" name={slagProps.scope.name}/>
-            <MDXRemote {...slagProps} />
-        </div>
+interface Props {
+  slagProps: CV;
+}
+
+const Cv = ({ slagProps: { compiledSource, scope } }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{scope.title}</title>
+      </Head>
+      <div className={styles.container}>
+        <Nav navLinkType="CUSTOM_NAV_LINK" name={scope.name} />
+        <MDXRemote compiledSource={compiledSource} scope={scope} />
+      </div>
     </>
-    )
+  )
 }
 
 
